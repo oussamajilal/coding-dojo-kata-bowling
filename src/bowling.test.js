479e85a3dd7d584c9e07bd9abec8011608ec49f6
Oussama Jilal <oussama.jilal@sahamassurance.com>
2021-05-05 17:15:38 +0000

    Corrected the number of rolls in unit tests

--- src/bowling.test.js
+++ src/bowling.test.js
@@ -2,10 +2,12 @@ import bowling from './bowling';
 
 describe('calculateScore', () => {
   it('should return 0 in case of all rolls are 0', () => {
-    expect(bowling.calculateScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(0);
+    const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(bowling.calculateScore(rolls)).toEqual(0);
   });
 
   it('should return correct score in case of failure of knocking all pins down', () => {
-    expect(bowling.calculateScore([1, 2, 5, 4, 0, 0, 3, 3, 9, 0])).toEqual(27);
+    const rolls = [1, 2, 5, 4, 0, 0, 3, 3, 9, 0, 1, 2, 5, 4, 0, 0, 3, 3, 9, 0];
+    expect(bowling.calculateScore(rolls)).toEqual(54);
   });
 });
