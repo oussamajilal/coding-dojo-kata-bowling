527169e35b28e0311c0463459a6a1aa9dd348b92
Oussama Jilal <oussama.jilal@sahamassurance.com>
2021-05-05 17:42:30 +0000

    Added support for two rolls are equal 10 but not in the same frame

--- src/bowling.js
+++ src/bowling.js
@@ -2,13 +2,15 @@ import _ from 'lodash';
 
 const isSpare = (roll, followingRoll) => roll + followingRoll === 10;
 
+const isSecondRollInFrame = rollNumber => rollNumber % 2 === 1;
+
 const calculateScore = (rolls) => {
   return _.sum(
     rolls.map((roll, index) => {
-      if (isSpare(rolls[index - 1], roll)) return roll + rolls[index + 1];
+      if (isSecondRollInFrame(index) && isSpare(rolls[index - 1], roll)) return roll + rolls[index + 1];
       return roll;
     })
   );
 };
 
-export default { calculateScore, isSpare };
+export default { calculateScore, isSpare, isSecondRollInFrame };
--- src/bowling.test.js
+++ src/bowling.test.js
@@ -15,6 +15,11 @@ describe('calculateScore', () => {
     const rolls = [1, 2, 5, 4, 0, 0, 3, 7, 9, 0, 1, 2, 5, 4, 0, 0, 3, 3, 9, 0];
     expect(bowling.calculateScore(rolls)).toEqual(67);
   });
+
+  it('should not correct score in case two rolls are equal 10 but not in the same frame', () => {
+    const rolls = [1, 5, 5, 4, 0, 0, 3, 7, 9, 0, 1, 2, 5, 4, 0, 0, 3, 3, 9, 0];
+    expect(bowling.calculateScore(rolls)).toEqual(70);
+  });
 });
 
 describe('isSpare', () => {
@@ -23,5 +28,15 @@ describe('isSpare', () => {
   });
   it('should return false if sum is not 10', () => {
     expect(bowling.isSpare(3, 6)).toEqual(false);
+  });
+});
+
+describe('isSecondRollInFrame', () => {
+  it('should return false if roll number is pair', () => {
+    expect(bowling.isSecondRollInFrame(8)).toEqual(false);
+  });
+
+  it('should return true if roll number is even', () => {
+    expect(bowling.isSecondRollInFrame(5)).toEqual(true);
   });
 });
